02896c2df8047798bc42c1f0642cd36af9bda369
ArthurTRamos <arthurtrottmann17@gmail.com>
2026-05-21 19:50:51 -0300

    updated main-page

--- Lista-Telefonica/src/app/contact-box/contact-box.ts
+++ Lista-Telefonica/src/app/contact-box/contact-box.ts
@@ -6,4 +6,34 @@ import { Component } from '@angular/core';
   templateUrl: './contact-box.html',
   styleUrl: './contact-box.scss',
 })
-export class ContactBox {}
+export class ContactBox {
+  contacts: {name: string, number: string}[] = [
+    {
+      "name": "Art",
+      "number": "+55 19 950211842"
+    },
+    {
+      "name": "Mai",
+      "number": "+55 22 222"
+    }
+  ]
+
+  getInitialLetters(name: string) {
+    const words = name.split(" ");
+    const firstName = words[0];
+    const lastName = words[words.length - 1];
+
+    if(words.length == 1) {
+      if(firstName.length == 1)
+        return firstName[0] + firstName[0];
+      return firstName[0] + firstName[1];
+    }
+    
+    return firstName[0] + lastName[0];
+  }
+
+  whatsappLink(number: string) {
+    number = number.slice(1);
+    return(`http://wa.me/${number.replaceAll(' ', '')}`);
+  }
+}
